d9cc8c815f538c1440c67493ad3210a9ba86418b
stratsync-rohit <rohit@stratsync.ai>
2025-12-23 05:50:10 +0530

    update

--- client-v3/src/main.tsx
+++ client-v3/src/main.tsx
@@ -2,7 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import './index.css';
+
+
+import '../src/css/index.css';
+
+
+const personalCssPath = '/src/css/user-index.css';
+fetch(personalCssPath, { method: 'HEAD' })
+  .then((res) => {
+    if (res.ok) {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = personalCssPath;
+      document.head.appendChild(link);
+    }
+  })
+  .catch(() => {});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
